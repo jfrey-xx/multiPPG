81ff459563fe075dc0ab46876bb7fd7ae7c71d0d
mavincen@U-BORDEAUX.FR <mavincen@U-BORDEAUX.FR@6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa>
2015-02-16 16:25:15 +0000

    gestion webcam
    git-svn-id: https://services.emi.u-bordeaux.fr/projet/svn/kinect/trunk@30 6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
 import cv2
 from Tkinter import *
+import os.path
 
 selectCam = Tk()
-
+selectCam.resizable(width=False, height=False)
+selectCam.title('Choix webcam')
+selectCam.geometry('{}x{}'.format(200, 70))
+selectCam.bind('<Escape>',exit)
 label = Label(selectCam, text="Selection de la camera")
 var_choix =IntVar()
-choix_1 = Radiobutton(selectCam, text="Camera 0", variable=var_choix, value=0, command=selectCam.quit)
-choix_2 = Radiobutton(selectCam, text="Camera 1", variable=var_choix, value=1, command=selectCam.quit)
-choix_1.pack()
-choix_2.pack()
+i=0
+while os.path.exists("/dev/video%d"%i):
+	choix = Radiobutton(selectCam, text="Camera %d"%i, variable=var_choix, value=i, command=selectCam.quit)
+	choix.pack()
+	i+=1
 label.pack()
 selectCam.mainloop()
 selectCam.destroy()
 
 cap = cv2.VideoCapture(var_choix.get())
-
-while True :
+while go :
 	ret,im = cap.read()
 	cv2.imshow('Mesure physiologique camera %d'%var_choix.get(),im)
 	key = cv2.waitKey(40)
+	print key
+	if key== 1048603 or key==27:
+		cv2.destroyAllWindows()
+		break
+
+	
