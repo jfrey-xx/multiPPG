518cedb4847ac36be66a00f8d32ced223576ed9c
mavincen@U-BORDEAUX.FR <mavincen@U-BORDEAUX.FR@6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa>
2015-02-16 11:05:35 +0000

    modif detection webcam
    git-svn-id: https://services.emi.u-bordeaux.fr/projet/svn/kinect/trunk@29 6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa

--- test.py
+++ test.py
@@ -1,23 +1,21 @@
 import cv2
 from Tkinter import *
 
+selectCam = Tk()
 
-# fenetre = Tk()
-# champ_label = Label(fenetre, text="contenu de notre champ label")
-# var_choix = IntVar()
-# choix_rouge = Radiobutton(fenetre, text="Camera 0", variable=var_choix, value=0)
-# choix_vert = Radiobutton(fenetre, text="Camera 1", variable=var_choix, value=1)
-# choix_rouge.pack()
-# choix_vert.pack()
-# champ_label.pack()
-cap = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(1)
+label = Label(selectCam, text="Selection de la camera")
+var_choix =IntVar()
+choix_1 = Radiobutton(selectCam, text="Camera 0", variable=var_choix, value=0, command=selectCam.quit)
+choix_2 = Radiobutton(selectCam, text="Camera 1", variable=var_choix, value=1, command=selectCam.quit)
+choix_1.pack()
+choix_2.pack()
+label.pack()
+selectCam.mainloop()
+selectCam.destroy()
+
+cap = cv2.VideoCapture(var_choix.get())
 
 while True :
 	ret,im = cap.read()
-	cv2.imshow('Mesure physiologique 0',im)
+	cv2.imshow('Mesure physiologique camera %d'%var_choix.get(),im)
 	key = cv2.waitKey(40)
-	ret2,im2 = cap2.read()
-	cv2.imshow('Mesure physiologique 1',im2)
-	key2 = cv2.waitKey(40)
-# fenetre.mainloop()
