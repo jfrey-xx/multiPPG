e69a80b6e482ddabc80b309e3c287b093c6c48a1
jeanbarey@U-BORDEAUX.FR <jeanbarey@U-BORDEAUX.FR@6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa>
2015-02-23 18:01:04 +0000

    interface graphique
    git-svn-id: https://services.emi.u-bordeaux.fr/projet/svn/kinect/trunk@32 6ba7955c-d1ab-4fe7-883e-3bb5bd3cddaa

--- src/webcamSelect.py
+++ src/webcamSelect.py
@@ -1,47 +1,76 @@
-import cv2
 from Tkinter import *
+import cv2
+from PIL import Image, ImageTk
 import os.path
+font_title=('courier', 12,'bold')
+font_other=('courier', 10,'bold')
 def camSelect():
 	selectCam = Tk()
+	selectCam.config(bg='#AED9FA')
 	selectCam.resizable(width=False, height=False)
-	selectCam.title('Choix webcam')
+	selectCam.title('webcam choice')
 	selectCam.bind('<Escape>',exit)
-	label = Label(selectCam, text="Selection de la camera")
+	label = Label(selectCam, text="Select cam")
+	label.config(font=font_title,bg='#AED9FA')
+	label.pack()
 	cam =IntVar()
-	i=0
-	while os.path.exists("/dev/video%d"%i):
-		choix = Radiobutton(selectCam, text="Camera {0}".format(i), variable=cam, value=i, command=selectCam.quit)
+	isok=True
+	i=1
+	while isok:
+		cap=cv2.VideoCapture(i)
+		isok=cap.isOpened()
+		choix = Radiobutton(selectCam, text="Camera {0}".format(i-1), variable=cam, value=i-1, command=selectCam.quit)
+		choix.config(font=font_other,bg='#AED9FA')
 		choix.pack()
 		i+=1
-	label.pack()
 	selectCam.geometry('{}x{}'.format(200, 50+10*i))
 	selectCam.mainloop()
 	selectCam.destroy()
 	return cam.get()
 
-def test():
-	print "coucou"	
-
 def main():
 	cam = camSelect()
 	cap = cv2.VideoCapture(cam)
-	# cap.set(3,1020) # set(ID, valeur), ou ID=3 pour la largeur et ID=4 pour la hauteur.
-	# cap.set(4,940)
-	while True:
-		ret,img = cap.read()
-		height, width = img.shape[:2]
-		cv2.putText(img, "Press ESC to close", (5, 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0,0,0))
-		cv2.putText(img, "Press s to begin process", (5, 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0,0,0))
-		cv2.imshow('Mesure physiologique camera : {0}'.format(cam),img)
-		key = cv2.waitKey(10)& 0xFF
-		# print key #affiche la valueur de la touche enfoncée
-		if key==27:	
-			cv2.destroyAllWindows()
-			break
-			cap.release()
-		if key == ord('s'):
-			test()
+	root = Tk()
 
+	# Sers seulement pour se reperer
+	root.config(bg='blue')
+
+
+	root.resizable(width=False,height=False)
+	root.title('Mesure physiologique camera : {0}'.format(0))
+	root.bind('<Escape>', lambda e: root.quit())
+
+	# Frame of the selection of measure (RIGHT)
+	cal_fram = Frame(root, borderwidth=1)
+	cal_fram.pack(side="right")
+	select_label = Label(cal_fram, text="Selection du calcul")
+	select_label.config(font=font_title)
+	select_label.pack(side="top")
+	cal = 0
+	measure = Radiobutton(cal_fram, text="Rythme Cardiaque", variable=cal, value=1)
+	measure.config(font=font_other)
+	measure.pack(side="right")
+	# Frame of key description(TOP)
+	label_frame = Frame(root, borderwidth=1)
+	label_frame.pack(anchor=W)
+	escape_label = Label(label_frame, text="Press ESC to close")
+	escape_label.config(font=font_title)
+	escape_label.pack(side="top")
+	lmain = Label(root)
+	lmain.pack()
+
+	def show_frame():
+		_, frame = cap.read()
+		frame = cv2.flip(frame, 1)
+		cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+		img = Image.fromarray(cv2image)
+		imgTK = ImageTk.PhotoImage(image=img)
+		lmain.imgTK = imgTK
+		lmain.configure(image=imgTK)
+		lmain.after(10, show_frame)
+	show_frame()
+	root.mainloop()
 
 if __name__ == '__main__':
 		main()	
