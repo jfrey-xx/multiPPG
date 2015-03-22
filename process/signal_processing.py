6589a7d4c524cbbf1b5c2a40091fb5a076087579
jfrey <nomhad@gmail.com>
2015-03-22 04:58:39 +0100

    Implement basic temporal filter.

--- process/signal_processing.py
+++ process/signal_processing.py
@@ -70,3 +70,40 @@ class GetMaxX(data.DataBuffer):
     # replace values with corresponding subdet
     self.push_values(sorted_x_values[0:self.queue_size])
 
+class TemporalFilter(data.SignalBuffer):
+  """
+  Use iFFT to remove unwanted frequencies
+  """
+  def __init__(self, input_signal_buffer, stop_list, window_length = -1, attach_plot = False, name = "TemporalFilter"):
+    """
+    input_signal_buffer: SignalBuffer type
+    stop_list: list of tuples, frequencies to remove. Eg [(0,4), (20,30), (60,-1)] to low-pass at 60, high-pass at 4 and notch between 20 and 30.
+    window_length: set parameter to extend window length of the signal in the buffer
+    """
+    self.stop_list = stop_list
+    # By default input buffer will be same length as the signal
+    if window_length <= 0:
+      window_length = input_signal_buffer.window_length
+      
+    # use a DataBuffer type because we store FFT, need fixed X axis (and don't use signal_processing.FFT because we need all points)
+    self.input_buffer = data.DataBuffer(input_signal_buffer.sample_rate, input_signal_buffer.sample_rate*window_length, input_data=input_signal_buffer)
+    self.input_buffer.set_x_values(scipy.fftpack.fftfreq(self.input_buffer.queue_size, 1./input_signal_buffer.sample_rate))
+    
+    data.SignalBuffer.__init__(self, self.input_buffer.sample_rate, input_signal_buffer.window_length, attach_plot = attach_plot, name = name)
+    self.input_buffer.add_callback(self)
+
+  def __call__(self, data_buffer, new_values):
+    # compute FFT over input buffer
+    fft = scipy.fft(self.input_buffer.values)
+    x = self.input_buffer.values_x
+    # zero selected frequencies
+    for stop in self.stop_list:
+      start = stop[0]
+      end = stop[1]
+      # "-1" code for upper bound
+      if end == -1:
+        end = np.max(x)
+      # zero selected band
+      fft[(abs(x) >= start) & (abs(x) <= end)] = 0
+    # compute iFFT and push    
+    self.push_values(scipy.ifft(fft).real)
